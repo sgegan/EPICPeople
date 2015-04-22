911a80b84d879dbcfbb014a09f2f06d6b45aa2be
Chris Svajlenka <chris@metaltoad.com>
2015-04-22 12:56:49 -0700

    Moved wordpress down into docroot/, added entries to .gitignore, linked wwebroot to docroot.

--- 2015/wp-content/plugins/cyclone-slider-2/src/CycloneSlider/Base.php
+++
@@ -1,8 +0,0 @@
-<?php
-abstract class CycloneSlider_Base {
-    protected $plugin;
-    
-    public function run( $plugin ) {
-        $this->plugin = $plugin;
-    }
-}
---
+++ docroot/wp-content/plugins/bp-direct-menus/inc/backend-noajax.inc.php
@@ -0,0 +1,82 @@
+<?php
+if( !defined( 'ABSPATH' ) )
+	die( 'Cheatin\' uh?' );
+
+/* Add a metabox in admin menu page */
+add_action('admin_head-nav-menus.php', 'bpdm_add_nav_menu_metabox');
+function bpdm_add_nav_menu_metabox() {
+	add_meta_box( 'bpdm', __( 'BuddyPress Direct Links' ) . ' v' . BPDIRECTMENU_VERSION, 'bpdm_nav_menu_metabox', 'nav-menus', 'side', 'default' );
+}
+
+/* The metabox code : Awesome code stolen from screenfeed.fr (GregLone) Thank you mate. */
+function bpdm_nav_menu_metabox( $object )
+{
+	global $nav_menu_selected_id;
+
+	$elems = array( '#bpdmlogin#' => __( 'Log In' ), 
+					'#bpdmlogout#' => __( 'Log Out' ), 
+					'#bpdmloginout#' => __( 'Log In' ).'|'.__( 'Log Out' ), 
+					'#bpdmregister#' => __( 'Register' ) ,
+					'#bpdmcustom#' => __( 'BuddyPress Direct Link' )
+				   );
+	class bpdmlogItems {
+		public $db_id = 0;
+		public $object = 'bpdmlog';
+		public $object_id;
+		public $menu_item_parent = 0;
+		public $type = 'custom';
+		public $title;
+		public $url;
+		public $target = '';
+		public $attr_title = '';
+		public $classes = array();
+		public $xfn = '';
+	}
+
+	$elems_obj = array();
+	foreach ( $elems as $value => $title ) {
+		$elems_obj[$title] = new bpdmlogItems();
+		$elems_obj[$title]->object_id	= esc_attr( $value );
+		$elems_obj[$title]->title		= esc_attr( $title );
+		$elems_obj[$title]->url			= esc_attr( $value );
+	}
+
+	$walker = new Walker_Nav_Menu_Checklist( array() );
+	?>
+	<div id="bp-direct-links" class="bpdirectlinksdiv">
+		<div id="tabs-panel-bp-direct-links-all" class="tabs-panel tabs-panel-view-all tabs-panel-active">
+			<ul id="bp-direct-linkschecklist" class="list:bp-direct-links categorychecklist form-no-clear">
+				<?php echo walk_nav_menu_tree( array_map( 'wp_setup_nav_menu_item', $elems_obj ), 0, (object)array( 'walker' => $walker ) ); ?>
+			</ul>
+		</div>
+
+		<p class="button-controls">
+			<span class="list-controls hide-if-no-js">
+				<a href="javascript:void(0);" class="help" onclick="jQuery( '#help-bp-direct-links' ).toggle();"><?php _e( 'Help' ); ?></a>
+				<span class="hide-if-js" id="help-bp-direct-links"><br /><a name="help-bp-direct-links"></a>
+					<?php
+						echo '&#9725; You can add a redirection page after the user\'s login/logout simply adding a relative link after the link\'s keyword, example <code>#bpdmloginout#index.php</code>.';
+						echo '<br />&#9725; You can also add <code>%actualpage%</code> to redirect the user on the actual visited page, example : <code>#bpdmloginout#%actualpage%</code>.';
+						echo '<br />&#9725; For the custom link, put the url-piece after the keyword, example : <code>#bpdmcustom#/activity/friends/</code> will redirect to: <code>http://%site%/members/%username%/activity/friends/</code>.  Custom links only show up when a user is logged in.';
+						echo '<br />%username%,%firstname%,%lastname%, %displayname%, %unreadmessagecount%, %friendcount%, %groupcount%, %(unreadmessagecount)%, %(friendcount)%, %(groupcount)%, %avatar%, %avatar-thumb%, %avatar-mini% will be replaced in the label.';
+					?>
+				</span>
+			</span>
+
+			<span class="add-to-menu">
+				<input type="submit"<?php disabled( $nav_menu_selected_id, 0 ); ?> class="button-secondary submit-add-to-menu right" value="<?php esc_attr_e('Add to Menu'); ?>" name="add-bp-direct-links-menu-item" id="submit-bp-direct-links" />
+				<span class="spinner"></span>
+			</span>
+		</p>
+	</div>
+	<?php
+}
+
+/* Modify the "type_label" */
+add_filter( 'wp_setup_nav_menu_item', 'bpdm_nav_menu_type_label' );
+function bpdm_nav_menu_type_label( $menu_item )
+{
+	if ( isset($menu_item->object, $menu_item->url) && $menu_item->object == 'custom' && strstr($menu_item->url, '#bpdm') )
+		$menu_item->type_label = ( 'BP Direct Link' );
+	return $menu_item;
+}
