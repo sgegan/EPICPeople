06968a7409945dbdecabeff7a8380baf7394c4c0
Chris Svajlenka <chris@metaltoad.com>
2015-05-07 15:04:48 -0700

    Making changes for digital ocean vps.

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,17 +1,19 @@
-set :stage_domain, "www.epicpeople.com"
+set :stage_domain, "epicpeople.org"
 
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/var/www/sites/virtual/#{application}-#{stage}"
+set :deploy_to, "/var/www/sites/virtual/epicpeople.org"
 
 # The hostnames to deploy to.
-role :web, "epicpeople.com"
+role :web, "104.236.169.126"
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "epicpeople.com", :primary => true
+role :db, "104.236.169.126", :primary => true
 
 # The path to wp-cli
 set :wp, "cd #{current_path}/#{app_root} ; /usr/local/bin/wp"
+
+set :httpd_group, "www-data"
 
 # The username on the target system, if different from your local username
 ssh_options[:user] = 'deploy'
