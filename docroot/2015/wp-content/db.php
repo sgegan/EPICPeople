c7f300e1d698fbb5b06b5046f1f4ef6df0a39fcd
Chris Svajlenka <chris@metaltoad.com>
2015-05-13 16:12:33 -0700

    2015 needs 2015 in its siteurl

--- docroot/2015/wp-content/db.php
+++ docroot/2015/wp-content/db.php
@@ -7,5 +7,5 @@ add_filter ( 'pre_option_home', 'use_normal_siteurl' );
 add_filter ( 'pre_option_siteurl', 'use_normal_siteurl' );
 function use_normal_siteurl( ) {
      $https = ($_SERVER['HTTPS'])? 's' : '';
-     return "http" .$https. "://" . $_SERVER['SERVER_NAME'];
+     return "http" .$https. "://" . $_SERVER['SERVER_NAME'] . '/2015/';
 }
