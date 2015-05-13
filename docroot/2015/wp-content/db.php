4b99546e573421ac0c4fec05066d4be4a36bc6a8
Chris Svajlenka <chris@metaltoad.com>
2015-05-13 15:16:00 -0700

    Adding db.php for setting siteurl for different domains.

---
+++ docroot/2015/wp-content/db.php
@@ -0,0 +1,11 @@
+<?php
+/*
+This allows us to use whatever domain we want for the siteurl
+*/
+
+add_filter ( 'pre_option_home', 'use_normal_siteurl' );
+add_filter ( 'pre_option_siteurl', 'use_normal_siteurl' );
+function use_normal_siteurl( ) {
+     $https = ($_SERVER['HTTPS'])? 's' : '';
+     return "http" .$https. "://" . $_SERVER['SERVER_NAME'];
+}
